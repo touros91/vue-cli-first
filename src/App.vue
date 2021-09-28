698ca64fac335e55111efef92540edf03b275f46
touros91 <andreageraci91@gmail.com>
2021-09-28 17:06:37 +0200

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
+  <div id="app">
     <Title msg="La mia prima app con Vue CLI!"/>
     <Hero title="Standard Tooling for Vue.js Development" subtitle="Vue CLI aims to be the standard tooling baseline for the Vue ecosystem. It ensures the various build tools work smoothly together with sensible defaults so you can focus on writing your app instead of spending days wrangling with configurations. At the same time, it still offers the flexibility to tweak the config of each tool without the need for ejecting."/>
-    <div class="image">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0giye6b65OcnCQAUbbkTsgK7OxT4ytFa75eNxU6P3UYNHcUgTqM06eZf4w4Wy3e7HH6M&usqp=CAU" alt="Vue.js CLI">
-    </div>
   </div>
 </template>
 
@@ -29,12 +26,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.image {
-  max-width: 250px;
-  margin: auto;
-}
-.image img {
-  width: 100%;
 }
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,6 +3,7 @@
         <div class="hero">
             <h2>{{ title }}</h2>
             <p>{{ subtitle }}</p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0giye6b65OcnCQAUbbkTsgK7OxT4ytFa75eNxU6P3UYNHcUgTqM06eZf4w4Wy3e7HH6M&usqp=CAU" alt="Vue.js CLI">
         </div>
     </div>
 </template>
